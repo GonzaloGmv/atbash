34fcf3b4226450ad0612395e6d43d6aa63fa5c14
carlospuigserver <cpuiggut@myuax.com>
2024-02-02 16:43:26 +0100

    comit

---
+++ español.py
@@ -0,0 +1,22 @@
+def cifrado_atbash(texto):
+    alfabeto_original = 'abcdefghijklmnopqrstuvwxyz'
+    alfabeto_clave = 'zyxwvutsrqponmlkjihgfedcba'
+    texto_cifrado = ''
+
+    for caracter in texto:
+        if caracter.lower() in alfabeto_original:
+            indice = alfabeto_original.index(caracter.lower())
+            if caracter.isupper():
+                texto_cifrado += alfabeto_clave[indice].upper()
+            else:
+                texto_cifrado += alfabeto_clave[indice]
+        else:
+            texto_cifrado += caracter
+
+    return texto_cifrado
+
+# Ejemplo de uso
+texto_original = "Hola Mundo"
+texto_cifrado = cifrado_atbash(texto_original)
+print("Texto original:", texto_original)
+print("Texto cifrado:", texto_cifrado)
